ab9706bc754195f2f40e8aa637ba2c726022e597
Fernando Uribe <furibe@bankingtech.com.ve>
2025-01-21 16:12:20 -0400

    Prueba de Vue

--- src/components/fields/SelectUnselectField.vue
+++ src/components/fields/SelectUnselectField.vue
@@ -1,19 +1,31 @@
 <template>
   <div style="width: 50%">
     <label>Available Options</label>
-    <select :id="field.id" :value="selected" multiple style="width: 100%">
-      <option
-        v-for="option in field.options"
-        :value="option.id"
-        :key="option.id"
-        @click="unselect($event)">
+    <select
+      :id="field.id"
+      :value="selected"
+      multiple
+      style="width: 100%"
+      @change="
+        handleChange;
+        unselect($event.target.value);
+      ">
+      <option v-for="option in field.options" :value="option.id" :key="option.id">
         {{ option.label }}
       </option>
     </select>
   </div>
   <div style="width: 50%">
     <label>Disabled Options</label>
-    <select :id="field.id" @change="handleChange;" :value="selected" multiple style="width: 100%">
+    <select
+      :id="field.id"
+      @change="
+        handleChange;
+        select($event.target.value);
+      "
+      :value="selected"
+      multiple
+      style="width: 100%">
       <option v-for="option in disabled" :value="option.id" :key="option.id">
         {{ option.label }}
       </option>
@@ -38,18 +50,14 @@ let { handleChange, debounce } = fieldMixin.setup(props, { emit });
 
 const unselect = (id) => {
   const found = props.field.options.find((option) => option.id == id);
-  disabled = props.field.filter((option) => option.id !== id);
-  props.field.options.push(found);
-
-  console.log(props.field.options, props.disabled);
+  props.field.options = props.field.options.filter((option) => option.id !== id);
+  props.disabled.push(found);
 };
 
 const select = (id) => {
   const found = props.disabled.find((option) => option.id == id);
-  disabled = props.disabled.filter((option) => option.id !== id);
+  props.disabled = props.disabled.filter((option) => option.id !== id);
   props.field.options.push(found);
-
-  console.log(props.field.options, props.disabled);
 };
 </script>
 
